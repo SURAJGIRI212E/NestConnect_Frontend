12af1b5f0a1a352e2830f0ecd8173e368545fbbf
SURAJGIRI212E <surajgiri212@gmail.com>
2025-10-20 16:31:42 +0530

    name changes

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -50,7 +50,7 @@ export const Register = () => {
         <div className="flex justify-center mb-6">
           <img src={logo} alt="X Logo" className="w-10" />
         </div>
-        <h1 className="text-2xl font-bold mb-6 text-center text-gray-800">Sign up for Twitter</h1>
+        <h1 className="text-2xl font-bold mb-6 text-center text-gray-800">Sign up for NestConnect</h1>
         {error && (
           <div className="mb-4 p-2 bg-red-400 bg-opacity-30 text-red-800 rounded-md">
             {error}
